c6d16acad0403a43b79bf06b9fc3ca13a1997231
sdman135 <53463830+sdman135@users.noreply.github.com>
2021-12-31 13:56:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IBM-Cloud-Data-Engineering---SQL-Queries
+ Putting into practice my IBM CloudPak Data Engineering skills.
---
+++ hide/IBM_Db2-credentials.py
@@ -0,0 +1,10 @@
+# pointing to my cloudpak DB2 host
+hostname = "54a2f15b-5c0f-46df-8954-7e38e612c2bd.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+port = 32733
+
+# Db2 credentials
+password = "m4KZ2cTooApRg3mA"
+username = "bvr08213"
+
+# name of database on IBM Cloud
+database = "bludb"
